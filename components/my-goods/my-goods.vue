c592ea43f4bf7b182a8a8da050d2a3f0a62f5809
1539042750 <1539042750@qq.com>
2022-10-28 14:14:34 +0800

    完成了购物车的开发

--- components/my-goods/my-goods.vue
+++ components/my-goods/my-goods.vue
@@ -2,12 +2,14 @@
   <view>
     <view class="goods-item">
       <view class="goods-item-left">
+        <radio :checked="goods.goods_state" color="#c00000" v-if="showRadio" @click="radioClickHandler"></radio>
         <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
       </view>
       <view class="goods-item-right">
         <view class="goods-name">{{goods.goods_name}}</view>
         <view class="goods-info-box">
           <view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+          <uni-number-box :min="1" :value="goods.goods_count"  @change="numChangeHandler" v-if="showNum"></uni-number-box>
         </view>
       </view>
     </view>
@@ -22,6 +24,14 @@
         type:Object,
         defaul:{},
       },
+      showRadio:{
+        type:Boolean,
+        default:false
+      },
+      showNum:{
+        type:Boolean,
+        default:false,
+      }
     },
     filters:{
       tofixed(num){
@@ -32,18 +42,37 @@
       return {
          defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
       };
+    },
+    methods:{
+      radioClickHandler(){
+        this.$emit('radio-change',{
+          goods_id: this.goods.goods_id,
+          goods_state: !this.goods.goods_state
+        })
+      },
+      numChangeHandler(val){
+        this.$emit('num-change',{
+          goods_id: this.goods.goods_id,
+          goods_count: +val
+        })
+      }
     }
   }
 </script>
 
 <style lang="scss">
 .goods-item{
+  width: 750rpx;
+  box-sizing: border-box;
   display: flex;
   padding: 10px 5px;
   border-bottom: 1px solid #f0f0f0;
   
   .goods-item-left{
     margin-right: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     
     .goods-pic{
       width: 100px;
@@ -59,6 +88,12 @@
     
     .goods-name{
       font-size: 13px;
+    }
+    
+    .goods-info-box{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
     }
     
     .good-price{
--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -22,124 +22,160 @@
     <rich-text :nodes="goods_info.goods_introduce"></rich-text>
     <!-- 商品导航组件 -->
     <view class="goods_nav">
-      <uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick" @buttonClick="buttonClick"></uni-goods-nav>
+      <uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick"
+        @buttonClick="buttonClick"></uni-goods-nav>
     </view>
   </view>
 </template>
 
 <script>
+  import {
+    mapMutations,
+    mapGetters
+  } from 'vuex'
   export default {
+    computed: {
+      ...mapGetters('m_cart', ['total']),
+    },
+    watch: {
+      total: {
+        handler(newVal) {
+          const findResult = this.options.find((x) => x.text === '购物车')
+          if (findResult) {
+            findResult.info = newVal
+          }
+        },
+        immediate: true
+      }
+    },
     data() {
       return {
-        goods_info:{},
-            options: [{
-              icon: 'shop',
-              text: '店铺'
-            }, {
-              icon: 'cart',
-              text: '购物车',
-              info: 2
-            }],
-            buttonGroup: [{
-                text: '加入购物车',
-                backgroundColor: '#ff0000',
-                color: '#fff'
-              },
-              {
-                text: '立即购买',
-                backgroundColor: '#ffa200',
-                color: '#fff'
-              }
-            ]
+        goods_info: {},
+        options: [{
+          icon: 'shop',
+          text: '店铺'
+        }, {
+          icon: 'cart',
+          text: '购物车',
+          info: 2
+        }],
+        buttonGroup: [{
+            text: '加入购物车',
+            backgroundColor: '#ff0000',
+            color: '#fff'
+          },
+          {
+            text: '立即购买',
+            backgroundColor: '#ffa200',
+            color: '#fff'
+          }
+        ]
       };
     },
-    onLoad(options){
-      const goods_id =options.goods_id
+    onLoad(options) {
+      const goods_id = options.goods_id
       this.getGoodsDetail(goods_id)
     },
-    methods:{
-      async getGoodsDetail(goods_id){
-        const {data:res} =await uni.$http.get('/api/public/v1/goods/detail', { goods_id })
-         if(res.meta.status !==200) return uni.$showMsg()
-        res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display:block;" ').replace(/webp/g, 'jpg')
-         this.goods_info=res.message
+    methods: {
+      ...mapMutations('m_cart', ['addToCart']),
+      async getGoodsDetail(goods_id) {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/detail', {
+          goods_id
+        })
+        if (res.meta.status !== 200) return uni.$showMsg()
+        res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display:block;" ')
+          .replace(/webp/g, 'jpg')
+        this.goods_info = res.message
       },
       // 实现轮播图的预览效果
-      preview(i){
+      preview(i) {
         uni.previewImage({
-          current:i,
-          urls:this.goods_info.pics.map(x=>x.pics_big)
+          current: i,
+          urls: this.goods_info.pics.map(x => x.pics_big)
         })
       },
       onClick(e) {
         if (e.content.text === '购物车') {
-          // 切换到购物车页面
           uni.switchTab({
             url: '/pages/cart/cart'
           })
         }
       },
-      buttonClick(){
-        console.log(111)
+      buttonClick(e) {
+        if (e.content.text === '加入购物车'){
+          const goods = {
+              goods_id: this.goods_info.goods_id,
+              goods_name: this.goods_info.goods_name,
+              goods_price: this.goods_info.goods_price,
+              goods_count: 1,
+              goods_small_logo: this.goods_info.goods_small_logo,
+              goods_state: true
+            }
+          this.addToCart(goods)
+        }
       }
     }
   }
 </script>
 
 <style lang="scss">
-swiper{
-  height: 750rpx;
-  
-  image{
-    width: 100%;
-    height: 100%;
-  }
-}
-.goods-info-box{
-  padding: 10px;
-  padding-right: 0;
-  
-  .price{
-    color: #c00000;
-    font-size: 18px;
-    margin: 10px 0;
-  }
-  
-  .goods-info-body{
-    display: flex;
-    justify-content: space-between;
-    
-    .goods-name{
-      font-size: 13px;
-      padding-right: 10px;
+  swiper {
+    height: 750rpx;
+
+    image {
+      width: 100%;
+      height: 100%;
     }
-    
-    .favi{
-      width: 120px;
-      font-size: 12px;
+  }
+
+  .goods-info-box {
+    padding: 10px;
+    padding-right: 0;
+
+    .price {
+      color: #c00000;
+      font-size: 18px;
+      margin: 10px 0;
+    }
+
+    .goods-info-body {
       display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      border-left: 1px solid #efefef;
+      justify-content: space-between;
+
+      .goods-name {
+        font-size: 13px;
+        padding-right: 10px;
+      }
+
+      .favi {
+        width: 120px;
+        font-size: 12px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border-left: 1px solid #efefef;
+        color: gray;
+      }
+    }
+
+    .yf {
+      margin: 10px 0;
+      font-size: 12px;
       color: gray;
     }
   }
-  
-  .yf{
-    margin: 10px 0;
-    font-size: 12px;
-    color: gray;
-  }
-}
-.goods-detail-container {
-  padding-bottom: 50px;
-}
 
-.goods_nav {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-}
+  .goods-detail-container {
+    padding-bottom: 50px;
+  }
+
+  .goods_nav {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
 </style>
